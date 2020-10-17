904ff06500eb483549be9c97d32b6eafe4a5fce5
Kjell Otto EXT <info@kjell-otto.de>
2020-10-17 08:42:47 +0200

    feat: add notes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+// TASK: Separate into components
+
 export const App = () => (
   <div className="app">
     <div className="navbar">Navbar</div>
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,9 +5,19 @@ import { App } from './App';
 // import CSS just as if it was a normal file
 import './index.css';
 
+
+// TASK: Understand Components and how to pass attributes
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// QUESTIONS:
+//   - What is a Component?
+//   - How to pass attributes / compose them?
+// LINKS:
+//   - Components: https://reactjs.org/docs/components-and-props.html
+
