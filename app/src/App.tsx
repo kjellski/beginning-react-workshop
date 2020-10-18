498c60f931745b8458dc1b10621a990802a54722
Kjell Otto EXT <info@kjell-otto.de>
2020-10-18 12:49:17 +0200

    feat: separated components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { FunctionComponent } from "react";
+import "./App.css";
 
 // TASK: Separate into components
 
+const Navbar: FunctionComponent = ({ children }): JSX.Element => (
+  <div className="navbar">{children}</div>
+);
+
+const NavbarHeader: FunctionComponent = ({ children }): JSX.Element => (
+  <div className="navbar-header">{children}</div>
+);
+
+const NavbarItem: FunctionComponent = ({ children }): JSX.Element => (
+  <div className="navbar-item">{children}</div>
+);
+
+const NavbarFooter: FunctionComponent = ({ children }): JSX.Element => (
+  <div className="navbar-footer">{children}</div>
+);
+
+const MainContent: FunctionComponent = ({ children }): JSX.Element => (
+  <div className="content">{children}</div>
+);
+
 export const App = () => (
   <div className="app">
-    <div className="navbar">
-      <div className="navbar-header">
-        Navbar
-      </div>
+    <Navbar>
+      <NavbarHeader>Navbar</NavbarHeader>
 
-      <div className="navbar-item">
-        A
-      </div>
-      <div className="navbar-item">
-        B
-      </div>
-      <div className="navbar-item">
-        C
-      </div>
-      
-      <div className="navbar-footer">
-        Profile
-      </div>
-    </div>
-    <div className="content">Content</div>
+      <NavbarItem>A</NavbarItem>
+      <NavbarItem>B</NavbarItem>
+      <NavbarItem>C</NavbarItem>
+
+      <NavbarFooter>Profile</NavbarFooter>
+    </Navbar>
+    <MainContent>Content</MainContent>
   </div>
 );
